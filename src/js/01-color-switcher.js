413b84507a67167c4c5aeb831475bd3aaa76b16f
oleksii-zviezdin <oleksii.zviezdin@gmail.com>
2023-03-18 23:53:25 +0100

    the task-9.1 is done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
 
+const startBtnEl = document.querySelector(`button[data-start]`);
+const stopBtnEl = document.querySelector(`button[data-stop]`);
+stopBtnEl.setAttribute(`disabled`, `true`);
+
+startBtnEl.addEventListener(`click`, handleStartBtnEl);
+stopBtnEl.addEventListener(`click`, handleStoptBtnEl);
+
+function handleStartBtnEl(event) {
+    startBtnEl.setAttribute(`disabled`, `true`);
+    stopBtnEl.removeAttribute(`disabled`);
+    console.log(startBtnEl);
+    timerId = setInterval(() => {
+        const color = getRandomHexColor();
+        document.body.style.backgroundColor = color;
+    }, 1000);
+}
+
+function handleStoptBtnEl(event) {
+    startBtnEl.removeAttribute(`disabled`);
+    stopBtnEl.setAttribute(`disabled`, `true`);
+    console.log(stopBtnEl);
+    clearInterval(timerId);
+}
